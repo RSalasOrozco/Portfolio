cef30f097ac7b2a0949b5386100139004602a1c7
Rodolfo De las Salas <desweb.rs@gmail.com>
2024-10-22 19:15:46 -0500

    correccion de las propiedades SVG

--- portfolio/src/components/Footer/Footer.tsx
+++ portfolio/src/components/Footer/Footer.tsx
@@ -31,25 +31,25 @@ export default function Footer() {
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-dasharray="32"
-                    stroke-dashoffset="32"
+                    strokeDasharray="32"
+                    strokeDashoffset="32"
                     d="M12 4c1.67 0 2.61 0.4 3 0.5c0.53 -0.43 1.94 -1.5 3.5 -1.5c0.34 1 0.29 2.22 0 3c0.75 1 1 2 1 3.5c0 2.19 -0.48 3.58 -1.5 4.5c-1.02 0.92 -2.11 1.37 -3.5 1.5c0.65 0.54 0.5 1.87 0.5 2.5c0 0.73 0 3 0 3M12 4c-1.67 0 -2.61 0.4 -3 0.5c-0.53 -0.43 -1.94 -1.5 -3.5 -1.5c-0.34 1 -0.29 2.22 0 3c-0.75 1 -1 2 -1 3.5c0 2.19 0.48 3.58 1.5 4.5c1.02 0.92 2.11 1.37 3.5 1.5c-0.65 0.54 -0.5 1.87 -0.5 2.5c0 0.73 0 3 0 3"
                   >
                     <animate
                       fill="freeze"
-                      attributeName="stroke-dashoffset"
+                      attributeName="strokeDashoffset"
                       dur="1.05s"
                       values="32;0"
                     />
                   </path>
                   <path
-                    stroke-dasharray="10"
-                    stroke-dashoffset="10"
+                    strokeDasharray="10"
+                    strokeDashoffset="10"
                     d="M9 19c-1.406 0-2.844-.563-3.688-1.188C4.47 17.188 4.22 16.157 3 15.5"
                   >
                     <animate
@@ -60,7 +60,7 @@ export default function Footer() {
                     />
                     <animate
                       fill="freeze"
-                      attributeName="stroke-dashoffset"
+                      attributeName="strokeDashoffset"
                       begin="1.2s"
                       dur="0.3s"
                       values="10;0"
@@ -97,16 +97,10 @@ export default function Footer() {
                 height="20"
                 viewBox="0 0 24 24"
               >
-                <circle
-                  cx="4"
-                  cy="4"
-                  r="2"
-                  fill="currentColor"
-                  fill-opacity="0"
-                >
+                <circle cx="4" cy="4" r="2" fill="currentColor" fillOpacity="0">
                   <animate
                     fill="freeze"
-                    attributeName="fill-opacity"
+                    attributeName="fillOpacity"
                     dur="0.225s"
                     values="0;1"
                   />
@@ -114,44 +108,40 @@ export default function Footer() {
                 <g
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="4"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="4"
                 >
-                  <path
-                    stroke-dasharray="12"
-                    stroke-dashoffset="12"
-                    d="M4 10v10"
-                  >
+                  <path strokeDasharray="12" strokeDashoffset="12" d="M4 10v10">
                     <animate
                       fill="freeze"
-                      attributeName="stroke-dashoffset"
+                      attributeName="strokeDashoffset"
                       begin="0.225s"
                       dur="0.3s"
                       values="12;0"
                     />
                   </path>
                   <path
-                    stroke-dasharray="12"
-                    stroke-dashoffset="12"
+                    strokeDasharray="12"
+                    strokeDashoffset="12"
                     d="M10 10v10"
                   >
                     <animate
                       fill="freeze"
-                      attributeName="stroke-dashoffset"
+                      attributeName="strokeDashoffset"
                       begin="0.675s"
                       dur="0.3s"
                       values="12;0"
                     />
                   </path>
                   <path
-                    stroke-dasharray="24"
-                    stroke-dashoffset="24"
+                    strokeDasharray="24"
+                    strokeDashoffset="24"
                     d="M10 15c0 -2.76 2.24 -5 5 -5c2.76 0 5 2.24 5 5v5"
                   >
                     <animate
                       fill="freeze"
-                      attributeName="stroke-dashoffset"
+                      attributeName="strokeDashoffset"
                       begin="0.975s"
                       dur="0.3s"
                       values="24;0"
@@ -175,9 +165,9 @@ export default function Footer() {
                 <path
                   fill="none"
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
                   d="m3 21l7.548-7.548M21 3l-7.548 7.548m0 0L8 3H3l7.548 10.452m2.904-2.904L21 21h-5l-5.452-7.548"
                   color="currentColor"
                 />
